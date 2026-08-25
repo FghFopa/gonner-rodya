e2670239fd56b083619cb903c49a1d8261091c90
fghfopa <fghfopa@gmail.com>
2026-08-25 22:01:47 +0500

    wffffffff

--- index.service.worker.js
+++ index.service.worker.js
@@ -1,8 +1,7 @@
-const CACHE_VERSION = '1787677089';
+const CACHE_VERSION = '1787677281';
 const CACHE_PREFIX = 'guster-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = 'index.offline.html';
-const ENSURE_CROSSORIGIN_ISOLATION_HEADERS = true;
 
 const CACHED_FILES = ["index.html","index.js","index.offline.html","index.icon.png","index.apple-touch-icon.png","index.audio.worklet.js","index.audio.position.worklet.js"];
 const CACHEABLE_FILES = ["index.wasm.br", "index.pck"];
@@ -18,30 +17,9 @@ self.addEventListener('activate', (event) => {
 	).then(() => ('navigationPreload' in self.registration) ? self.registration.navigationPreload.enable() : Promise.resolve()));
 });
 
-function ensureCrossOriginIsolationHeaders(response) {
-	if (response.headers.get('Cross-Origin-Embedder-Policy') === 'require-corp'
-		&& response.headers.get('Cross-Origin-Opener-Policy') === 'same-origin') {
-		return response;
-	}
-	const crossOriginIsolatedHeaders = new Headers(response.headers);
-	crossOriginIsolatedHeaders.set('Cross-Origin-Embedder-Policy', 'require-corp');
-	crossOriginIsolatedHeaders.set('Cross-Origin-Opener-Policy', 'same-origin');
-	return new Response(response.body, {
-		status: response.status,
-		statusText: response.statusText,
-		headers: crossOriginIsolatedHeaders,
-	});
-}
-
 async function fetchAndCache(event, request, cache, isCacheable) {
-	let response = await event.preloadResponse;
-	if (response == null) {
-		response = await self.fetch(request);
-	}
-	if (ENSURE_CROSSORIGIN_ISOLATION_HEADERS) {
-		response = ensureCrossOriginIsolationHeaders(response);
-	}
-	if (isCacheable) {
+	let response = await event.preloadResponse || await self.fetch(request);
+	if (response && response.status === 200 && isCacheable) {
 		cache.put(request, response.clone());
 	}
 	return response;
@@ -65,29 +43,18 @@ self.addEventListener('fetch', (event) => {
 	if (isNavigate || isCacheable) {
 		event.respondWith((async () => {
 			const cache = await caches.open(CACHE_NAME);
-			if (isNavigate) {
-				const fullCache = await Promise.all(FULL_CACHE.map((name) => cache.match(name)));
-				if (fullCache.some((v) => v === undefined)) {
-					try {
-						return await fetchAndCache(event, req, cache, isCacheable);
-					} catch (e) {
-						return caches.match(OFFLINE_URL);
-					}
-				}
-			}
 			let cached = await cache.match(req);
 			if (cached != null) {
-				if (ENSURE_CROSSORIGIN_ISOLATION_HEADERS) {
-					cached = ensureCrossOriginIsolationHeaders(cached);
-				}
 				return cached;
 			}
-			return await fetchAndCache(event, req, cache, isCacheable);
-		})());
-	} else if (ENSURE_CROSSORIGIN_ISOLATION_HEADERS) {
-		event.respondWith((async () => {
-			let response = await fetch(req);
-			return ensureCrossOriginIsolationHeaders(response);
+			try {
+				return await fetchAndCache(event, req, cache, isCacheable);
+			} catch (e) {
+				if (isNavigate) {
+					return caches.match(OFFLINE_URL);
+				}
+				throw e;
+			}
 		})());
 	}
 });
